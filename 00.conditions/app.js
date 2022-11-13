b2518b9000a15e38085bd0f7e33ec2f111f5e319
Wolf Developer <lupo.andrea.93@gmail.com>
2022-11-13 11:11:01 +0100

    key on v-for

--- 00.conditions/app.js
+++ 00.conditions/app.js
@@ -9,6 +9,9 @@ const app = Vue.createApp({
     addGoal () {
       this.goals.push(this.inputGoal);
       console.log(this.goals);
+    },
+    removeGoal (index) {
+      this.goals.splice(index,1);
     }
   }
 });
