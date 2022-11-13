ffac1b0fdfa44fce8ddf46d939e05bde6b37abfe
Wolf Developer <lupo.andrea.93@gmail.com>
2022-11-13 10:19:28 +0100

    v-if and v-for

---
+++ 00.conditions/app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+  data() {
+    return { 
+      goals: [],
+      inputGoal: '',
+    };
+  },
+  methods: {
+    addGoal () {
+      this.goals.push(this.inputGoal);
+      console.log(this.goals);
+    }
+  }
+});
+
+app.mount('#user-goals');
